cb4726193494b31093975a34e74b07a983c49324
Daniel <60996796+dbudiii@users.noreply.github.com>
2024-12-25 10:43:14 -0800

    Updated. Need to fetch channel ids and store

--- tg-scrape.py
+++ tg-scrape.py
@@ -1,2 +1,27 @@
 # tg-scrape.py
 
+import config
+
+from telethon.sync import TelegramClient
+import asyncio
+
+api_id = config.api_id
+api_hash = config.api_hash
+
+client = TelegramClient('tg-scrape', api_id, api_hash)
+
+async def main():
+    await client.start()
+
+    # Get all chat ids
+    chats = await client.get_dialogs()
+    chat_ids = {}
+
+    # Store them in a dictionary (both title and id)
+    for chat in chats:
+        chat_ids = chat.id
+        
+    print(chat_ids)
+
+with client:
+    client.loop.run_until_complete(main())
