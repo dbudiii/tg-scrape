f53c13dd1b08f2472cc807a21040ea7ba666a8cd
Daniel <60996796+dbudiii@users.noreply.github.com>
2025-01-06 01:02:56 -0800

    Added SQLite3 boilerplate. Need to add the parsing logic in messages next.

--- tg-scrape.py
+++ tg-scrape.py
@@ -4,12 +4,31 @@ import config
 
 from telethon.sync import TelegramClient
 import asyncio
+import sqlite3
 
+# Replace your API ID and API Hash
 api_id = config.api_id
 api_hash = config.api_hash
 
+# Connect to the SQLite database
+conn = sqlite3.connect('tg-scrape.db')
+cursor = conn.cursor()
+
+# Create the tokens table if it doesn't exist. Will add more columns later on
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tokens (
+        id INTEGER PRIMARY KEY,
+        chat_id INTEGER,
+        message_text TEXT,
+        token_name TEXT,
+        token_symbol TEXT
+    )
+''')
+
+# Create a TelegramClient
 client = TelegramClient('tg-scrape', api_id, api_hash)
 
+# Authenticate the client
 async def main():
     await client.start()
 
@@ -23,11 +42,13 @@ async def main():
         
     # Fetch messages from each chat
     for chat_tittle, chat_id in chat_ids.items():
-        messages = await client.get_messages(chat_id)
+        messages = await client.get_messages(chat_id, limit=100)
         for message in messages:
 
             # Insert logic to parse and store messages in database here
             print(message.text)
+
+            await asyncio.sleep(1)
 
 with client:
     client.loop.run_until_complete(main())
