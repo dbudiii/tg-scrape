ad8afeb1d92dbc1a795279df43ac2a71dc58448f
Daniel <60996796+dbudiii@users.noreply.github.com>
2025-01-07 00:52:22 -0800

    Started adding the logic to parse messages and store in database. Need to update the dictionaries next time

--- tg-scrape.py
+++ tg-scrape.py
@@ -5,6 +5,7 @@ import config
 from telethon.sync import TelegramClient
 import asyncio
 import sqlite3
+import re
 
 # Replace your API ID and API Hash
 api_id = config.api_id
@@ -18,12 +19,13 @@ cursor = conn.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS tokens (
         id INTEGER PRIMARY KEY,
+        chat_name TEXT,
         chat_id INTEGER,
         message_text TEXT,
-        token_name TEXT,
         token_symbol TEXT
     )
 ''')
+
 
 # Create a TelegramClient
 client = TelegramClient('tg-scrape', api_id, api_hash)
@@ -37,16 +39,30 @@ async def main():
     chat_ids = {}
 
     # Store them in a dictionary (both title and id)
+    # May need to update this and the above so that we are storing both the id and title as values in the dictionary
     for chat in chats:
         chat_ids[chat.title] = chat.id
         
     # Fetch messages from each chat
-    for chat_tittle, chat_id in chat_ids.items():
+    # Need to update this if we are updating the dictionary pulls above
+    for chat_title, chat_id in chat_ids.items():
         messages = await client.get_messages(chat_id, limit=100)
         for message in messages:
 
-            # Insert logic to parse and store messages in database here
-            print(message.text)
+            # Parse the messages and extract token mentions starting with "$"
+            token_mentions = re.findall(r'\$\w+', message.text)
+
+            # Store token mentions into database
+            for token in token_mentions:
+                cursor.execute('''
+                               INSERT INTO tokens (chat_name, chat_id, message_text, token_symbol)
+                               VALUES (?, ?, ?, ?)
+                               ''', (chat_title, chat_id, message.text,token[1:]))
+                conn.commit()
+
+            rows = cursor.fetchall()
+            for row in rows:
+                print(row)
 
             await asyncio.sleep(1)
 
