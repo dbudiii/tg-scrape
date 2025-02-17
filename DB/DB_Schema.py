8ad273ccee1931cb00743d7f1da38f291557e0a4
Daniel | Laptop <60996796+dbudiii@users.noreply.github.com>
2025-02-17 00:54:10 -0800

    Updated function to allow for table name editing

--- DB/DB_Schema.py
+++ DB/DB_Schema.py
@@ -1,13 +1,13 @@
 # DB Schema
 
 # Create the tokens schema if it doesn't exist [EDIT SCHEMA]
-def create_table(cursor):
+def create_table(cursor, table_name):
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS tokens (
+        CREATE TABLE IF NOT EXISTS {} (
         id INTEGER PRIMARY KEY,
         chat_name TEXT,
         chat_id INTEGER,
         message_text TEXT,
         token_symbol TEXT
         )
-        ''')
+        '''.format(table_name))
