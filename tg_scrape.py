c41b48ffd6379fb217b9bb7fd839b66f6879258f
Daniel | Laptop <60996796+dbudiii@users.noreply.github.com>
2025-02-17 00:52:38 -0800

    Updated main function to reduce token functions

--- tg_scrape.py
+++ tg_scrape.py
@@ -16,7 +16,7 @@ tg_api_hash = config.tg_api_hash
 hel_api_key = config.hel_api_key
 
 # Create a TelegramClient
-client = TelegramClient('tg-scrape', tg_api_id, tg_api_hash)
+client = TelegramClient('tg_scrape', tg_api_id, tg_api_hash)
 
 # Authenticate the client
 async def main():
@@ -26,26 +26,12 @@ async def main():
     messages = await fetch_messages(client)
 
     for message in messages:
-            # Parse the messages and extract token addresses and put into list
-            coin_addresses = extract_addresses(message)
+        # Parse the messages and extract token addresses and put into list
+        coin_addresses = extract_addresses(message)
 
-    # Connect to the database from the DB_Connection file and create the table    
-    cursor = get_connection()
-    
-    
-                # Calculate token FDV
-                fdv = calculate_token_fdv(address, get_token_price(address), get_token_supply(address))
-                print(fdv)
-
-                insert_data(cursor, chat_data['title'], chat_id, message.text, token[1:])
-
-            # Print the results
-            print_data(cursor)
-
-            await asyncio.sleep(1)
-
-    # Close the database connection
-    conn.close()
+    test_list = []
+    for address in coin_addresses:
+        get_coin_symbol(address)
 
 with client:
     client.loop.run_until_complete(main())
