51e5004e5f391bda2464a357bc5cd26b4305cf76
Daniel | Laptop <60996796+dbudiii@users.noreply.github.com>
2025-03-15 01:42:16 +0800

    Latest with some pats of DB code

--- tg_scrape.py
+++ tg_scrape.py
@@ -28,14 +28,6 @@ async def main():
         addresses = extract_addresses(message.text)
         coin_addresses.extend(addresses)
 
-    # [Need to store the coin symbols into the database, pull the financial data, then display***]
-    
-    # # Codeblock to pull coin data
-    # coin_data_dict = {}
-    # for address in coin_addresses:
-    #     coin_data = get_coin_data(address)
-    #     coin_data_dict[address] = coin_data
-
 if __name__ == '__main__':
     # Create connection to database
     cursor = get_connection()
@@ -44,8 +36,10 @@ if __name__ == '__main__':
     table_name = 'tokens'
     create_table(cursor, table_name)
 
-    read_table(cursor, table_name)
-
     # Execute main loop
     with client:
         client.loop.run_until_complete(main())
+
+    # Close connection
+    conn.commit()
+    conn.close()
