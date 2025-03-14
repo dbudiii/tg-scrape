8b68b948f1fd69d35707938d2be36bda3072cb38
Daniel | Laptop <60996796+dbudiii@users.noreply.github.com>
2025-03-14 17:43:06 +0800

    Updated schema so we are just using token address

--- DB/DB_Schema.py
+++ DB/DB_Schema.py
@@ -5,9 +5,6 @@ def create_table(cursor, table_name):
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS {} (
         id INTEGER PRIMARY KEY,
-        chat_name TEXT,
-        chat_id INTEGER,
-        message_text TEXT,
         token_address TEXT
         )
         '''.format(table_name))
