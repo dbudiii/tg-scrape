f8085816384d30a0814bba84de273cc4304b606a
Daniel | Laptop <60996796+dbudiii@users.noreply.github.com>
2025-03-14 17:42:42 +0800

    Updated conn to be a global variable outsite of just the function so we can use conn.commit and close

--- DB/DB_Connection.py
+++ DB/DB_Connection.py
@@ -2,9 +2,9 @@
 import sqlite3
 
 DB_PATH = 'tg_scrape.db'
+conn = sqlite3.connect(DB_PATH)
 
 # Connect to the SQLite database
 def get_connection():
-    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     return cursor 
