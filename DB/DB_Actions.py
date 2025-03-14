81f88230232d31aed66e9bb4094ff450a7db679a
Daniel | Laptop <60996796+dbudiii@users.noreply.github.com>
2025-03-14 09:39:34 +0800

    Fixed import and updated the read_table function. Will have to update the rest

--- DB/DB_Actions.py
+++ DB/DB_Actions.py
@@ -1,10 +1,10 @@
 # DB Actions
 
 import sqlite3
-from DB_Connection import get_connection
+from DB.DB_Connection import *
 
 # Takes an SQL query input and makes it easy to execute
-def execute_query(query):
+def execute_query(cursor, query):
     try:
         cursor.execute(query)
         result = cursor.fetchall()
@@ -19,19 +19,24 @@ def drop_table(table_name):
     return execute_query(query)
 
 # Read the table, executed through execute_query
-def read_table(table_name):
+def read_table(cursor, table_name):
     query = "SELECT * FROM {table_name};".format(table_name=table_name)
-    results = execute_query(query)
-    for row in results:
-        print(row)
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except sqlite3.Error as e:
+        print(f"Error executing query: {e}")
+        return None
 
 # Inserting data into table through execute_query [UPDATE]
-def insert_data(cursor, chat_name, chat_id, message_text, token_symbol):
+def insert_data(cursor, chat_name, chat_id, message_text, token_address):
     try:
         cursor.execute('''
-                    INSERT INTO tokens (chat_name, chat_id, message_text, token_symbol)
+                    INSERT INTO tokens (chat_name, chat_id, message_text, token_address)
                     VALUES (?, ?, ?, ?)
-                    ''', (chat_name, chat_id, message_text,token_symbol))
-        conn.commit()
+                    ''', (chat_name, chat_id, message_text,token_address))
+        cursor.commit()
     except sqlite3.Error as e:
         print(f"Error inserting data: {e}")
