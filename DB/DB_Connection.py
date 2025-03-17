74a1931cbec6e773fa618c5842bbeec0794d1842
Daniel <60996796+dbudiii@users.noreply.github.com>
2025-03-16 19:25:38 -0700

    Removed the quick alter script from here to the Schema file

--- DB/DB_Connection.py
+++ DB/DB_Connection.py
@@ -1,7 +1,6 @@
 # DB Connection
 import sqlite3
 
-
 DB_PATH = 'tg_scrape.db'
 conn = sqlite3.connect(DB_PATH)
 
@@ -9,11 +8,3 @@ conn = sqlite3.connect(DB_PATH)
 def get_connection():
     cursor = conn.cursor()
     return cursor 
-
-# To alter the table schema
-# cursor = get_connection()
-# cursor.execute('''CREATE TABLE tokens_new (id INTEGER PRIMARY KEY, token_address TEXT, timestamp TEXT)''')
-# cursor.execute('''DROP TABLE tokens''')
-# cursor.execute('''ALTER TABLE tokens_new RENAME TO tokens''')
-# conn.commit()
-# conn.close()
