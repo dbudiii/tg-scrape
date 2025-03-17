55488593676741babe991b177605b5415e67c58c
Daniel <60996796+dbudiii@users.noreply.github.com>
2025-03-16 18:43:59 -0700

    Added section to alter table schema quickly if needed, though commented out

--- DB/DB_Connection.py
+++ DB/DB_Connection.py
@@ -1,6 +1,7 @@
 # DB Connection
 import sqlite3
 
+
 DB_PATH = 'tg_scrape.db'
 conn = sqlite3.connect(DB_PATH)
 
@@ -8,3 +9,11 @@ conn = sqlite3.connect(DB_PATH)
 def get_connection():
     cursor = conn.cursor()
     return cursor 
+
+# To alter the table schema
+# cursor = get_connection()
+# cursor.execute('''CREATE TABLE tokens_new (id INTEGER PRIMARY KEY, token_address TEXT, timestamp TEXT)''')
+# cursor.execute('''DROP TABLE tokens''')
+# cursor.execute('''ALTER TABLE tokens_new RENAME TO tokens''')
+# conn.commit()
+# conn.close()
