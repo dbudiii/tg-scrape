47003bb737cf4174f50f33cbdab25eff5fc3c431
Daniel <60996796+dbudiii@users.noreply.github.com>
2025-03-16 19:25:11 -0700

    Updated table to add the UNIQUE constraint. Also moved the quick alter script here.

--- DB/DB_Schema.py
+++ DB/DB_Schema.py
@@ -1,4 +1,5 @@
 # DB Schema
+from DB_Connection import *
 
 # Create the tokens schema if it doesn't exist
 def create_table(cursor, table_name):
@@ -6,7 +7,16 @@ def create_table(cursor, table_name):
         CREATE TABLE IF NOT EXISTS {} (
         id INTEGER PRIMARY KEY,
         token_address TEXT,
-        timestamp TEXT
+        timestamp TEXT,
+        UNIQUE (token_address, timestamp)
         )
         '''.format(table_name))
     
+
+# To alter the table schema
+# cursor = get_connection()
+# cursor.execute('''CREATE TABLE tokens_new (id INTEGER PRIMARY KEY, token_address TEXT, timestamp TEXT, UNIQUE (token_address, timestamp))''')
+# cursor.execute('''DROP TABLE tokens''')
+# cursor.execute('''ALTER TABLE tokens_new RENAME TO tokens''')
+# conn.commit()
+# conn.close()
